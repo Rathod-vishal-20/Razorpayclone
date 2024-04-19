2acb4aec04ba71a0b665ba5bc0c71bd254996e42
Vishal Rathod <vishalrathod191918@gmail.com>
2024-04-19 21:46:38 +0530

    Razorpay clone

---
+++ index.js
@@ -0,0 +1,1 @@
+console.log('Namaste duniya to kaise ho aap log')
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish: ["Mulish", "sans-serif"],
+      },
+      colors:{
+        deepBlue: "#02042a",
+      }
+    },
+  },
+  plugins: [],
+}
+
